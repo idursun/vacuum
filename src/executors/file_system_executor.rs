a53b06e5e85753796b7dc2a202860ada2c81fe8d
ibrahim.dursun <ibrahim.dursun@gmail.com>
2019-08-02 15:26:36 +0100

    Use context methods instead of duplicating the logic

--- src/executors/file_system_executor.rs
+++ src/executors/file_system_executor.rs
@@ -23,38 +23,28 @@ where
 
 impl Ops for FileSystemExecutor<PathBuf> {
     fn copy<S: AsRef<str>>(&self, file_name: S) {
-        let mut source = self.source.clone();
-        source.push(file_name.as_ref());
+        let source = self.source.sub(file_name.as_ref());
         if !source.exists() {
             return;
         }
 
-        let mut destination = self.target.clone();
-        if fs::create_dir_all(destination.as_path()).is_ok() {
-            destination.push(file_name.as_ref());
+        if fs::create_dir_all(self.target.as_path()).is_ok() {
+            let destination = self.target.sub(file_name.as_ref());
             fs::copy(source.as_path(), destination.as_path()).expect("failed to copy");
         }
     }
 
     fn copy_glob<S: AsRef<str>>(&self, pattern: S) {
-        let full_pattern = format!(
-            "{}{}{}",
-            self.source.to_str().unwrap(),
-            std::path::MAIN_SEPARATOR,
-            pattern.as_ref()
-        );
-        for entry in glob::glob(full_pattern.as_ref()).unwrap() {
-            if let Ok(path) = entry {
-                let current = path.strip_prefix(self.source.as_path()).unwrap();
-                let mut destination = self.target.clone();
-                destination.push(current);
+        for path in self.source.search(pattern.as_ref()) {
+            if path.is_dir() {
+                continue;
+            }
+            let current = path.strip_prefix(self.source.as_path()).unwrap();
+            let target = self.target.sub(current.to_str().unwrap());
+            let dest_dir = target.parent().unwrap();
 
-                println!(
-                    "copying files matching {} from {} to {}",
-                    pattern.as_ref(),
-                    path.display(),
-                    destination.display()
-                );
+            if let Ok(_) = fs::create_dir_all(&dest_dir) {
+                fs::copy(path.as_path(), target.as_path()).expect("failed to copy");
             }
         }
     }
@@ -70,30 +60,22 @@ impl Ops for FileSystemExecutor<PathBuf> {
 
 impl Context for FileSystemExecutor<PathBuf> {
     fn home(&self) -> Self {
-        let mut destination = self.target.clone();
-        destination.push("home");
         FileSystemExecutor {
             source: self.source.home(),
-            target: destination,
+            target: self.target.sub("home"),
         }
     }
 
     fn config(&self) -> Self {
-        let mut destination = self.target.clone();
-        destination.push("config");
         FileSystemExecutor {
             source: self.source.config(),
-            target: destination,
+            target: self.target.sub("config"),
         }
     }
 
     fn sub<S: AsRef<str>>(&self, sub: S) -> Self {
-        let FileSystemExecutor {
-            mut source,
-            mut target,
-        } = self.clone();
-        source.push(sub.as_ref());
-        target.push(sub.as_ref());
+        let source = self.source.sub(sub.as_ref());
+        let target = self.target.sub(sub.as_ref());
 
         FileSystemExecutor { source, target }
     }
@@ -103,12 +85,8 @@ impl Context for FileSystemExecutor<PathBuf> {
         let sources = self.source.search(pattern);
         for source in sources {
             let remaining = source.strip_prefix(self.source.as_path()).unwrap();
-            let mut new_destination = self.target.clone();
-            new_destination.push(remaining);
-            ret.push(FileSystemExecutor {
-                source,
-                target: new_destination,
-            })
+            let target = self.target.sub(remaining.to_str().unwrap());
+            ret.push(FileSystemExecutor { source, target })
         }
         ret
     }
