8f0d372d2d1741044986583caa794c4995f6e3d6
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-08-16 11:11:15 +0100

    Convert Webstorm config to a vacuum file

--- src/main.rs
+++ src/main.rs
@@ -56,31 +56,6 @@ fn goland() -> App {
     }
 }
 
-fn webstorm() -> App {
-    App {
-        name: "webstorm".into(),
-        actions: vec![Action::Context(
-            Folder::Home,
-            vec![Action::Context(
-                Folder::Search(".WebStorm*".into()),
-                vec![Action::Context(
-                    Folder::Custom("config".into()),
-                    vec![
-                        Action::Context(
-                            Folder::Custom("keymaps".into()),
-                            vec![Action::CopyGlob("*.xml".into())],
-                        ),
-                        Action::Context(
-                            Folder::Custom("options".into()),
-                            vec![Action::Copy("editor.xml".into())],
-                        ),
-                    ],
-                )],
-            )],
-        )],
-    }
-}
-
 fn main() {
     let dir = std::fs::read_dir("./apps/").unwrap();
     for entry in dir {
