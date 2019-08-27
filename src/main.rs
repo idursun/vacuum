17137a8838e04e49a3577c57645073fd66f1dff0
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-08-27 10:42:24 +0100

    Extract vacuum file parsing to a method

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,13 @@ mod error;
 mod executors;
 mod parser;
 use crate::app::{Action, App, Folder};
+use crate::error::VacuumError;
 use crate::executors::{FileSystemContext, FileSystemExecutor};
 use std::fs;
 
-fn main() -> Result<(), error::VacuumError> {
+fn parse_vacuum_files() -> Result<Vec<App>, VacuumError> {
     let dir = std::fs::read_dir("./apps/")?.filter_map(Result::ok);
-
-    let output_folder = std::env::args()
-        .nth(1)
-        .unwrap_or_else(|| "output".to_owned());
-    let current_dir = std::env::current_dir()?;
+    let mut apps = Vec::new();
     for entry in dir {
         if !entry.file_type()?.is_file() {
             continue;
@@ -20,6 +17,18 @@ fn main() -> Result<(), error::VacuumError> {
 
         let content = fs::read_to_string(entry.path()).unwrap();
         let app = parser::parse(content)?;
+        apps.push(app);
+    }
+    Ok(apps)
+}
+
+fn main() -> Result<(), error::VacuumError> {
+    let output_folder = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "output".to_owned());
+    let current_dir = std::env::current_dir()?;
+    let apps = parse_vacuum_files()?;
+    for app in apps {
         let mut app_dir = current_dir.clone();
         app_dir.push(output_folder.clone());
         app_dir.push(&app.name);
