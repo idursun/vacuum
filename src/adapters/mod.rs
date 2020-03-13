3af8dcb142764bb8a1167353c9122fccdd2c9628
ibrahim.dursun <ibrahim.dursun@gmail.com>
2020-03-13 14:17:49 +0000

    Move use_cases to its own folder

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,12 +1,10 @@
 mod pom_parser;
-mod restore_usecase;
-mod store_usecase;
-mod deps_usecase;
+mod use_cases;
 
 pub mod context;
 pub mod executor;
 pub use pom_parser::PomParser;
 
-pub use restore_usecase::RestoreUseCase;
-pub use store_usecase::StoreUseCase;
-pub use deps_usecase::DepsUseCase;
+pub use use_cases::DepsUseCase;
+pub use use_cases::RestoreUseCase;
+pub use use_cases::StoreUseCase;
---
+++ src/adapters/use_cases.rs
@@ -0,0 +1,7 @@
+mod deps_usecase;
+mod restore_usecase;
+mod store_usecase;
+
+pub use deps_usecase::DepsUseCase;
+pub use restore_usecase::RestoreUseCase;
+pub use store_usecase::StoreUseCase;
